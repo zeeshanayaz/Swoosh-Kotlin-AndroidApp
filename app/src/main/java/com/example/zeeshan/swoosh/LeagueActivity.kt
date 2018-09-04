e91c67a90251d660436ab816973c57ab5b8a6249
Zeeshan Ayaz <zeeshanayaz1@gmail.com>
2018-09-04 22:09:25 +0500

    Skill Activity Layout Created

--- app/src/main/java/com/example/zeeshan/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/zeeshan/swoosh/LeagueActivity.kt
@@ -1,12 +1,19 @@
 package com.example.zeeshan.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    fun leagueNextClick(view : View){
+        val skillIntent = Intent(this, SkillActivity::class.java)
+        startActivity(skillIntent)
     }
 }
---
+++ app/src/main/java/com/example/zeeshan/swoosh/SkillActivity.kt
@@ -0,0 +1,12 @@
+package com.example.zeeshan.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SkillActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill)
+    }
+}

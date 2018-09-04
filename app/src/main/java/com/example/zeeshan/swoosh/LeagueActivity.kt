e9d74d942a81f12f2fef36c0015a7eb8ecc15528
Zeeshan Ayaz <zeeshanayaz1@gmail.com>
2018-09-04 21:44:27 +0500

    Done with Activity Lifecycle - Base Activity

--- app/src/main/java/com/example/zeeshan/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/zeeshan/swoosh/LeagueActivity.kt
@@ -3,7 +3,7 @@ package com.example.zeeshan.swoosh
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

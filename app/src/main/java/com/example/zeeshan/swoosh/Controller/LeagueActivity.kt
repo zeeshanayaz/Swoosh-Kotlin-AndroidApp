5540790fbdfc4fcb506eba2d9a10a8c851f78c3e
Zeeshan Ayaz <zeeshanayaz1@gmail.com>
2018-09-07 02:05:25 +0500

    Setting Instance State and App Icon

--- app/src/main/java/com/example/zeeshan/swoosh/Controller/LeagueActivity.kt
+++ app/src/main/java/com/example/zeeshan/swoosh/Controller/LeagueActivity.kt
@@ -2,6 +2,7 @@ package com.example.zeeshan.swoosh.Controller
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
 import com.example.zeeshan.swoosh.Model.Player
@@ -14,9 +15,22 @@ class LeagueActivity : BaseActivity() {
 //    var selectedLeague = ""
 
     var player = Player("","")
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if(savedInstanceState != null){
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
     }
 
     fun onMensClicked(view: View){
@@ -42,7 +56,7 @@ class LeagueActivity : BaseActivity() {
 
     fun leagueNextClick(view : View){
 
-        if(mensLeagueToggleBtn.isChecked == true || womensLeagueToggleBtn.isChecked == true || coEdLeagueToggleBtn.isChecked == true){
+        if(player.league != "" || mensLeagueToggleBtn.isChecked == true || womensLeagueToggleBtn.isChecked == true || coEdLeagueToggleBtn.isChecked == true){
             val skillIntent = Intent(this, SkillActivity::class.java)
             skillIntent.putExtra(EXTRA_PLAYER, player)
             startActivity(skillIntent)
--- app/src/main/java/com/example/zeeshan/swoosh/Controller/SkillActivity.kt
+++ app/src/main/java/com/example/zeeshan/swoosh/Controller/SkillActivity.kt
@@ -16,10 +16,22 @@ class SkillActivity : BaseActivity() {
 
     lateinit var player: Player
 
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER,player)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
         player = intent.getParcelableExtra(EXTRA_PLAYER)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if(savedInstanceState != null){
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
     }
 
 
